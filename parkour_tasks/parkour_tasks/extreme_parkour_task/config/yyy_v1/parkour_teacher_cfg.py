b9c2cb120fbd59814ccff2e036c0fd394b6169a6
Wenxi Xu <xuwenxi0517@gmail.com>
2025-10-27 12:27:22 +0800

    Fixed a warning

--- parkour_tasks/parkour_tasks/extreme_parkour_task/config/yyy_v1/parkour_teacher_cfg.py
+++ parkour_tasks/parkour_tasks/extreme_parkour_task/config/yyy_v1/parkour_teacher_cfg.py
@@ -17,7 +17,7 @@ class ParkourTeacherSceneCfg(ParkourDefaultSceneCfg):
     height_scanner = RayCasterCfg(
         prim_path="{ENV_REGEX_NS}/Robot/base_link",
         offset=RayCasterCfg.OffsetCfg(pos=(0.375, 0.0, 20.0)),
-        attach_yaw_only=True,
+        ray_alignment='yaw',
         pattern_cfg=patterns.GridPatternCfg(resolution=0.15, size=[1.65, 1.5]),
         debug_vis=False,
         mesh_prim_paths=["/World/ground"],
